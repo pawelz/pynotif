00afeeec1c7674f2b54b9007d9d3f286a9c1eeda
Paweł Zuzelski <pawelz@pld-linux.org>
2009-07-14 11:45:23 +0200

    Added shiftwidth=4 to vim modeline.

--- pynotif.py
+++ pynotif.py
@@ -1,4 +1,4 @@
-# vim:fileencoding=utf-8
+# vim:fileencoding=utf-8:sw=4
 
 #   This program is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
